4e239f23a5ec71f84aac7ebd5799d13b15a8a329
delta-leader <a0855909@unet.univie.ac.at>
2020-09-28 14:39:03 +0900

    new test setting

--- Classifier.py
+++ Classifier.py
@@ -497,10 +497,12 @@ if __name__ == "__main__":
     #scaler = GRFScaler(scalertype="standard")
     #class_dict = {"HC":0, "H":1, "K":1, "A":2, "C":2}
     #fetcher.set_class_dict(class_dict)
-    train = fetcher.fetch_set(raw=False, onlyInitial=True, dropOrthopedics="All", dropBothSidesAffected=False, dataset="TRAIN", stepsize=1, averageTrials=True, scaler=scaler, concat=True, val_setp=0, include_info=True, clip=False)
     val = fetcher.fetch_set(raw=False, onlyInitial=True, dropOrthopedics="All", dropBothSidesAffected=False, dataset="TRAIN_BALANCED", stepsize=1, averageTrials=True, scaler=scaler, concat=True, val_setp=0.2, include_info=True, clip=False)
-    test = fetcher.fetch_set(raw=False, onlyInitial=True, dropOrthopedics="All", dropBothSidesAffected=False, dataset="TEST", stepsize=1, averageTrials=True, scaler=scaler, concat=True, val_setp=0, include_info=True, clip=False)
-    train = set_valSet(train, val, parse="SESSION_ID")
+    scaler.reset()
+    train = fetcher.fetch_set(raw=False, onlyInitial=True, dropOrthopedics="All", dropBothSidesAffected=False, dataset="TRAIN_BALANCED", stepsize=1, averageTrials=False, scaler=scaler, concat=True, val_setp=0, include_info=True, clip=False)
+    
+    #test = fetcher.fetch_set(raw=False, onlyInitial=True, dropOrthopedics="None", dropBothSidesAffected=False, dataset="TEST", stepsize=1, averageTrials=True, scaler=scaler, concat=True, val_setp=0, include_info=True, clip=False)
+    train = set_valSet(train, val, parse=None)
 
     classifier = Classifier()
 
@@ -516,8 +518,8 @@ if __name__ == "__main__":
     #for key in ["affected", "non_affected"]:
     #    test[key] = img_data[key]
 
-    #classifier.predict("1DCNN-strided", test, val_set=False, boosting=False)
-    classifier.train_and_predict("MLP1", train, test=test, name=None, log=False, save_plot=False, show_plot=False, plot_architecture=False, boosting=False)
+    #classifier.predict("FCN-original", test, val_set=False, boosting=False)
+    classifier.train_and_predict("MLP1", train, name=None, log=False, save_plot=False, show_plot=False, plot_architecture=False, boosting=False)
     #train(self, model, data, deterministic=True, name=None, store=True, log=True, save_plot=False, show_plot=True, plot_architecture=False, loss=None, metrics=None, class_dict=None, filepath=None):
     #classifier.predict("models/output/MLP1/MLP1.h5", train, val_set=True, boosting=False)
     
